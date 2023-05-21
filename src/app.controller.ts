3a48f3c386cde6ec78be5429acb9268a0eb13493
Patrick <patrick@payro.io>
2023-05-21 16:57:38 +0000

    postsass initial setup

---
+++ src/app.controller.ts
@@ -0,0 +1,26 @@
+import { Controller, Get, Post } from '@nestjs/common';
+import { AppService } from './app.service';
+
+@Controller()
+export class AppController {
+  // is called dependency injection
+  constructor(private readonly appService: AppService) {}
+
+  @Get('')
+  getHello(): string {
+    return this.appService.getHello();
+  }
+
+
+  @Get('test')
+  getTestResult(): string {
+    return 'test hello';
+  }
+
+  @Post('/post_name')
+  postHello(): string {
+    return 'post i am all good to go'
+  }
+
+
+}
